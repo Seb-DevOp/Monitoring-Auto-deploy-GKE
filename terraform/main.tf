7148ccbaf267267a42f5057ea6375bf585151299
Seb GG <sebastien.giguet@ynov.com>
2025-07-13 15:24:58 +0200

    Fix_Nodesless

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@ provider "google" {
 
 resource "google_container_cluster" "primary" {
   name     = "gke-monitoring-cluster"
-  region  = null
   location = var.region
   deletion_protection = false
   remove_default_node_pool = true
